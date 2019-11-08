27a8c8ec94d5ac1524e6a135644e5972bd62e1da
nrvnrv <nrvnrv@github.com>
2019-11-09 01:33:13 +0300

    v3
    Рефакторинг (до процесса проверки чека - тк там еще будут правки)

--- tbot.py
+++ tbot.py
@@ -1,142 +1,131 @@
-# импорт библиотек
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from orderinfo import OrderInfo
 
 import requests
 import random
 from pyzbar.pyzbar import decode
 from PIL import Image
+
+
 # токен  бота
 API_TOKEN = '1056772125:AAFQrxSVgSzMO3Ihc9Rb3n4Uqm9pYZAa5NQ'
-
 # создание бота и диспетчера
-bot = Bot(token = API_TOKEN)
+bot = Bot(token=API_TOKEN)
 bot.parse_mode = 'HTML'
 dp = Dispatcher(bot)
 
+# order_chats[id чатa заказа][id того кто заказывает]=что он заказывает
+order_chats = {}  # все чаты которые делают заказ
+starter = {}  # те кто начинают чат (класс OrderInfo)
+# массивы выше мб можно объединить (по сути - это в бд поэтому не важно как оно здесь)
 
-# master_name - тот кто спровоцировал заказ (start)
-global order_info
-order_info = {'chat_id': '', 'master_name': '', 'master_id': ''}
+# костыли:
+order_enable = 0
+photo_enable = 0
 
+# kod[chat_id] = n-значный код?
+kod = {}
 
-global orders # заказы польз-й через makeorder
-orders = {}
-# бот запущен = 1 иначе 0 - нужно для проверки ниже
-global txt_en
-txt_en = 0
-
-firebase_admin.initialize_app(
-    credentials.Certificate('credentials.json')
-)
-db = firestore.client()
-
-
-#разрешить отпраить фото
-global ph_enable
-ph_enable = 0
-
-def get_inf(what):  # для получения...
-    MethodGetUpdates = 'https://api.telegram.org/bot{token}/getUpdates'.format(token=API_TOKEN)
-    response = requests.post(MethodGetUpdates)
-    results = response.json()
-    if what == 'user_id':  #...id пользователя
-        print(results)
-        return results['result'][0]['message']['from']['id']
-    if what == 'chat_id': #...id чата
-        return results['result'][0]['message']['chat']['id']
-    if what == 'user_name':    #...имени пользователя
-        return results['result'][0]['message']['from']['first_name']
-    if what == 'chat_name':  # ...имени чата
-        return results['result'][0]['message']['chat']['title'] \
-               or results['result'][0]['message']['chat']['first_name']
-    return -1
-
-
-def init(): # инициализация структуры при старте
-    if '' not in order_info.values(): # если уже был заказ, поля непустые
-        return 0
-    order_info['master_id'] = get_inf('user_id')
-    order_info['master_name'] = get_inf('user_name')
-    order_info['chat_id'] = get_inf('chat_id')
-    if -1 in order_info.values(): # если была ошибка получения инф-ии
-        return 0
-    return 1
-
-
-# обработик команды /start
+# обработчик команды /start
 @dp.message_handler(commands=['start'])
 async def if_start(message: types.Message):
 
-    if init(): # если заказ начинается, иниц-м главного заказчика и чат заказа
-        await bot.send_message(order_info['chat_id'], "Привет, будем заказывать\n<b>"\
-                               + str(order_info['master_name'])\
-                               + "</b> - инициатор заказа, будет иметь основные права и обязанности. Пишите места командой")
-        await bot.send_message(order_info['master_id'], "Привет, " + str(order_info['master_name']) \
-                               + " Ты решил сделать заказ в чате " + get_inf('chat_name') +" 😎 ")
-    else: # если заказ начат
-        await bot.send_message(order_info['chat_id'], get_inf('user_name')\
-                               + '! Заказ уже делается! Не хулигань. 😠')
+    global order_chats
 
-    some_data = {'event': 'send_start_message', 'user_id': message.from_user.id, 'message_id': message.message_id}
-    db.collection('events').add(some_data)
-    await bot.send_message(get_id(), "Привет!\nПомогу тебе и твоей группе упростить процесс заказа.")
+    # если старт пишешь в личку боту
+    if message.chat.type == 'private':
+        message_text = "%s, привет. " \
+                       "Команда /help поможет разобраться как что работает" % message.from_user.first_name
+        await bot.send_message(message.chat.id, message_text)
+        return
+
+    # когда старт был нажат уже кем-то
+    if message.chat.id in order_chats:
+        message_text = "%s! Заказ уже делается! Не хулигань. 😠" % message.from_user.first_name
+        await bot.send_message(message.chat.id, message_text)
+        return
+
+    # при первом старте запоминаем чат, назначаем главного
+    order_chats[message.chat.id] = {}
+    starter[message.chat.id] = OrderInfo(message) # пока не используется, потом
+    message_text = "Привет, будем заказывать\n" \
+                   "<b>%s</b> - инициатор заказа, будет иметь основные права и обязанности. " \
+                   "Пишите места командой" % message.from_user.first_name
+    await bot.send_message(message.chat.id, message_text)
+
+    # информируем главного что он главный
+    message_text = "Привет, %s! Ты решил сделать заказ в чате %s \n 😎 " % (message.from_user.first_name, message.chat.title)
+    await bot.send_message(message.from_user.id, message_text)
+
+    # генерируем код
+    kod[message.chat.id] = random.randint(1000, 9999)  # пока не используется, потом
 
 
-
-
+# обработчик команды /makeorder
 @dp.message_handler(commands=['makeorder'])
 async def if_makeorder(message: types.Message):
-    id_tmp = get_inf('user_id')
-    chat_tmp = get_inf('chat_id')
-    # заказ уже кто-то начал => в словаре order_info не может быть '':
-    if '' not in order_info.values():
-        if id_tmp not in orders: # если юзер еще не делал заказ
-            orders[id_tmp]={} # запоминаем тех кто заказывает
-            await bot.send_message(chat_tmp, "<b>" +\
-                                   get_inf('user_name') + "</b> делает заказ")
-            await bot.send_message(message.from_user.id, "Заказ через команду /eat")
-        else: # если юзер делал заказ
-            await bot.send_message(message.from_user.id, 'Заказывай же!')
-    else: # заказ net
-        await bot.send_message(chat_tmp, 'Заказ еще не начат')
 
-@dp.message_handler(content_types=['text'])
-async def eat_message(msg: types.Message):
-    global txt_en
-    if msg.text == '/eat':
-        txt_en = 1
-        await bot.send_message(msg.from_user.id, 'Пиши пункты заказа через перенос')
+    global order_chats
+
+    # польз-ль уже жал makeorder
+    if message.from_user.id in order_chats[message.chat.id]:
+        await bot.send_message(message.from_user.id, 'Заказывай же!')
         return
-    elif msg.text == '/bill':
-        global ph_enable
-        ph_enable = 1
-        await bot.send_message(msg.from_user.id, 'Отправь фото чека\nФото должно быть четким')
+
+    # запоминаем того кто заказывает
+    order_chats[message.chat.id][message.from_user.id] = {}
+    message_text = "<b>%s</b> делает заказ" % message.from_user.first_name
+    await bot.send_message(message.chat.id, message_text)
+    message_text = "Заказ через команду /eat. " \
+                "Уникальный код чата %s" % kod[message.chat.id]
+    await bot.send_message(message.from_user.id, message_text)
+
+
+# обработчик команды /eat & /bill
+@dp.message_handler(content_types=['text'])
+async def if_message(message: types.Message):
+
+    global order_chats
+    global order_enable
+    global photo_enable
+
+    # командой eat разрешаем ввод заказа текстом
+    if message.text == '/eat':
+        # проверка что пользователь регистрировался в чате - жал makeorder
+        for i in order_chats:
+            if message.from_user.id in order_chats[i]:
+                order_enable = 1
+                await bot.send_message(message.from_user.id, 'Пиши пункты заказа через перенос')
+                return
+        # если не нашли пользователя в массиве
+        await bot.send_message(message.from_user.id, "Вас нет в списках! Жмите makeorder в чате заказа")
+        return
+
+    # командой bill разрешаем отправку фото
+    elif message.text == '/bill':
+        photo_enable = 1
+        await bot.send_message(message.from_user.id, 'Отправь фото чека\nФото должно быть четким')
+
+        # если просто текст поступил с разрешенным txt_enable
     else:
-        if txt_en:
-            txt_en = 0
-            if get_inf('user_id') not in orders:
-                await bot.send_message(msg.from_user.id, "Вас нет в списках! Жмите makeorder в чате заказа")
-            else:
-                tmp_id = get_inf('user_id')
-                tmp_name = get_inf('user_name')
-                orders[tmp_id][tmp_name] = {}
-                for i in range(len(msg.text.split())):
-                    orders[tmp_id][tmp_name][i] = msg.text.split()[i]
-                to_out = []
-                for i in orders[tmp_id][tmp_name]:
-                    to_out.append('<b>'+str(i) + '</b> - ' + str(orders[tmp_id][tmp_name][i]))
-                await bot.send_message(msg.from_user.id, "\n".join(to_out))
-
-
-
-
-
+        if order_enable:
+            order_enable = 0
+            tmp = 0
+            for i in order_chats:
+                if message.from_user.id in order_chats[i]:
+                    tmp = i
+                    break
+            for i in range(len(message.text.split())):
+                order_chats[tmp][message.from_user.id][i] = message.text.split()[i]
+            to_out = []
+            for i in order_chats[tmp][message.from_user.id]:
+                to_out.append('<b>' + str(i) + '</b> - ' + str(order_chats[tmp][message.from_user.id][i]))
+            await bot.send_message(message.from_user.id, "\n".join(to_out))
+        else:
+            await bot.send_message(message.from_user.id, "/help  ⬅  жми")
 
 
 
@@ -169,8 +158,8 @@ def bill_detal_inf(fn, fd, fpd):
 
 @dp.message_handler(content_types=['photo'])
 async def handle_docs_photo(message):
-    global ph_enable
-    if ph_enable:
+    global photo_enable
+    if photo_enable:
         try:
             file_id = message.photo[len(message.photo)-1].file_id
             site = 'https://api.telegram.org/bot' \
@@ -208,6 +197,5 @@ async def handle_docs_photo(message):
         await bot.send_message(message.chat.id, 'К чему ты это?', reply_to_message_id=message)
 
 
-
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
